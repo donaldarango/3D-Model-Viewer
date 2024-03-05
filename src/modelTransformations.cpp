51f5ecc4240cf7163565e4d7c76d9ea4cee06e66
darango3002 <donaldarango@ufl.edu>
2024-03-05 01:22:05 -0500

    can read in obj files and color triangles

--- src/modelTransformations.cpp
+++ src/modelTransformations.cpp
@@ -3,26 +3,57 @@
 #define GLFW_INCLUDE_NONE
 #include <GLFW/glfw3.h>
 
+#define GLM_ENABLE_EXPERIMENTAL
+#include <glm/glm.hpp>
+#include <glm/gtx/transform.hpp>
+#include <glm/vec3.hpp>
+#include <glm/vec4.hpp>
+#include <glm/mat4x4.hpp>
+#include <glm/gtc/type_ptr.hpp> 
+#include <glm/gtx/string_cast.hpp>
+
 #include <iostream>
+#include <fstream>
+#include <string>
+#include <sstream>
+#include <vector>
+#include <algorithm>
+
+struct Vertex {
+    float x, y, z;
+};
 
 void framebuffer_size_callback(GLFWwindow* window, int width, int height);
 void processInput(GLFWwindow *window);
+std::vector<GLfloat> readObjFile(const std::string& filename, std::vector<Vertex>& vertices, size_t& numVertices);
 
 // settings
 const unsigned int SCR_WIDTH = 800;
 const unsigned int SCR_HEIGHT = 600;
 
+
 const char *vertexShaderSource = "#version 330 core\n"
     "layout (location = 0) in vec3 aPos;\n"
+    "layout (location = 1) in vec3 vertexColors;\n"
+    "uniform mat4 model;\n"
+    "out vec3 v_vertexColors;\n"
     "void main()\n"
     "{\n"
+    "   v_vertexColors = vertexColors;\n"
     "   gl_Position = vec4(aPos.x, aPos.y, aPos.z, 1.0);\n"
     "}\0";
+// const char *vertexShaderSource = "#version 330 core\n"
+//     "layout (location = 0) in vec3 aPos;\n"
+//     "void main()\n"
+//     "{\n"
+//     "   gl_Position = vec4(aPos.x, aPos.y, aPos.z, 1.0);\n"
+//     "}\0";
 const char *fragmentShaderSource = "#version 330 core\n"
+    "in vec3 v_vertexColors;\n"
     "out vec4 FragColor;\n"
     "void main()\n"
     "{\n"
-    "   FragColor = vec4(1.0f, 0.5f, 0.2f, 1.0f);\n"
+    "   FragColor = vec4(v_vertexColors.r, v_vertexColors.g, v_vertexColors.b, 1.0f);\n"
     "}\n\0";
 
 int main()
@@ -48,7 +79,7 @@ int main()
         return -1;
     }
     glfwMakeContextCurrent(window);
-    glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
+    // glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
 
     // // glew: load all OpenGL function pointers
     glewInit();
@@ -94,38 +125,104 @@ int main()
     glDeleteShader(vertexShader);
     glDeleteShader(fragmentShader);
 
+    // glm::mat4 model(1.0f);
+    // model = glm::scale(model, glm::vec3(0.5f,0.5f,0.5f));
+    // std::cout << glm::to_string(model[0]) << std::endl;
+    // std::cout << glm::to_string(model[1]) << std::endl;
+    // std::cout << glm::to_string(model[2]) << std::endl;
+    // std::cout << glm::to_string(model[3]) << std::endl;
+
+    // glm::vec4 worldspace_vec = (model * glm::vec4(-1.0f,-1.0f, 1.0f, 1.0f));
+    // std::cout << "\n";
+    // std::cout << "World Space" << std::endl;
+    // std::cout << glm::to_string(worldspace_vec) << std::endl;
+
+    // GLuint location = glGetUniformLocation(shaderProgram, "model");
+    // glUniformMatrix4fv(location, 1, false, glm::value_ptr(model));
+
+    // hardcoded for now
+    std::string filename = "/Users/donny/Documents/Model Transformations/data/triangles.obj"; 
+    std::vector<Vertex> verticesVector;
+    size_t numVertices = 0;
+    std::vector<GLfloat> vertices = readObjFile(filename, verticesVector, numVertices);
+
+    const std::vector<GLfloat> vertexColors
+    {
+        1.0f, 0.0f, 0.0f,
+        0.0f, 1.0f, 0.0f,
+        0.0f, 0.0f, 1.0f,
+        1.0f, 0.0f, 0.0f,
+        0.0f, 1.0f, 0.0f,
+        0.0f, 0.0f, 1.0f,
+        1.0f, 0.0f, 0.0f,
+        0.0f, 1.0f, 0.0f,
+        0.0f, 0.0f, 1.0f,
+    };
+
+    // if (faceData) {
+    //     std::cout << "Number of vertices: " << numVertices << std::endl;
+    //     std::cout << "Number of faces: " << numFaces << std::endl;
+    //     std::cout << "Face data:" << std::endl;
+    //     for (size_t i = 0; i < numFaces * 9; ++i) {
+    //         std::cout << faceData.at(i) << " ";
+    //         if ((i + 1) % 9 == 0)
+    //             std::cout << std::endl;
+    //     }
+    // }
+    
+
     // set up vertex data (and buffer(s)) and configure vertex attributes
     // ------------------------------------------------------------------
-    float vertices[] = {
-        -0.5f, -.25f, 0.0f, // left  
-         0.5f, -.75f, 0.0f, // right 
-         0.0f,  0.5f, 0.0f, // top   
+    // float vertices[] = {
+    //     -0.5f, -.25f, 0.0f, // left  
+    //      0.5f, -.75f, 0.0f, // right 
+    //      0.0f,  0.5f, 0.0f, // top   
 
-        -0.5f, -0.5f, 0.0f, // left  
-         0.5f, -0.5f, 0.0f, // right 
-         0.0f, -1.0f, 0.0f  // bottom
-    };
-    unsigned int numVertices = sizeof(vertices)/3;
+    //     -0.5f, -0.5f, 0.0f, // left  
+    //      0.5f, -0.5f, 0.0f, // right 
+    //      0.0f, -1.0f, 0.0f  // bottom
+    // };
+    // unsigned int numVertices = sizeof(vertices)/3;
 
-    unsigned int VBO, VAO;
+
+    unsigned int VBO, VBO2, VAO;
     glGenVertexArrays(1, &VAO);
     glGenBuffers(1, &VBO);
     // bind the Vertex Array Object first, then bind and set vertex buffer(s), and then configure vertex attributes(s).
     glBindVertexArray(VAO);
 
     glBindBuffer(GL_ARRAY_BUFFER, VBO);
-    glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+    // glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+    glBufferData(GL_ARRAY_BUFFER, vertices.size() *  sizeof(GL_FLOAT), vertices.data(), GL_STATIC_DRAW);
 
-    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 0, (void*)0);
     glEnableVertexAttribArray(0);
 
     // note that this is allowed, the call to glVertexAttribPointer registered VBO as the vertex attribute's bound vertex buffer object so afterwards we can safely unbind
     glBindBuffer(GL_ARRAY_BUFFER, 0); 
 
+    // For colors
+    glGenBuffers(1, &VBO2);
+    glBindBuffer(GL_ARRAY_BUFFER, VBO2);
+    glBufferData(GL_ARRAY_BUFFER, 
+                 vertexColors.size() * sizeof(GL_FLOAT),
+                 vertexColors.data(),
+                 GL_STATIC_DRAW);
+    
+    glEnableVertexAttribArray(1);
+    glVertexAttribPointer(1,
+                          3, // r,g,b
+                          GL_FLOAT,
+                          GL_FALSE,
+                          0, // stride
+                          (void*)0);    
+
     // You can unbind the VAO afterwards so other VAO calls won't accidentally modify this VAO, but this rarely happens. Modifying other
     // VAOs requires a call to glBindVertexArray anyways so we generally don't unbind VAOs (nor VBOs) when it's not directly necessary.
-    glBindVertexArray(0); 
+    glBindVertexArray(0);
 
+    glDisableVertexAttribArray(0);
+    glDisableVertexAttribArray(1);
 
     // uncomment this call to draw in wireframe polygons.
     // glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
@@ -140,13 +237,14 @@ int main()
 
         // render
         // ------
-        glClearColor(0.0f, 0.0f, 0.0f, 0.0f);
+        glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
         glClear(GL_COLOR_BUFFER_BIT);
 
         // draw our first triangle
         glUseProgram(shaderProgram);
         glBindVertexArray(VAO); // seeing as we only have a single VAO there's no need to bind it every time, but we'll do so to keep things a bit more organized
-        glDrawArrays(GL_TRIANGLES, 0, numVertices);
+        // glDrawArrays(GL_TRIANGLES, 0, numVertices); 
+        glDrawArrays(GL_TRIANGLES, 0, vertices.size()); 
         // glBindVertexArray(0); // unbind our VA no need to unbind it every time 
  
         // glfw: swap buffers and poll IO events (keys pressed/released, mouse moved etc.)
@@ -159,6 +257,7 @@ int main()
     // ------------------------------------------------------------------------
     glDeleteVertexArrays(1, &VAO);
     glDeleteBuffers(1, &VBO);
+    glDeleteBuffers(1, &VBO2);
     glDeleteProgram(shaderProgram);
 
     // glfw: terminate, clearing all previously allocated GLFW resources.
@@ -182,4 +281,47 @@ void framebuffer_size_callback(GLFWwindow* window, int width, int height)
     // make sure the viewport matches the new window dimensions; note that width and 
     // height will be significantly larger than specified on retina displays.
     glViewport(0, 0, width, height);
+}
+
+std::vector<GLfloat> readObjFile(const std::string& filename, std::vector<Vertex>& vertices, size_t& numVertices) {    
+    std::ifstream file(filename);
+    if (!file.is_open()) {
+        std::cerr << "Error opening file: " << filename << std::endl;
+        return std::vector<GLfloat>();
+    }
+
+    std::vector<GLfloat> faceData;
+    std::string line;
+    while (std::getline(file, line)) {
+        std::istringstream iss(line);
+        std::string prefix;
+        iss >> prefix;
+
+        if (prefix == "v") {
+            Vertex vertex;
+            iss >> vertex.x >> vertex.y >> vertex.z;
+            vertices.push_back(vertex);
+            numVertices++;
+        } else if (prefix == "f") {
+            int v1, v2, v3;
+            char slash, trash;
+            iss >> v1 >> slash >> slash >> trash;
+            iss >> v2 >> slash >> slash >> trash;
+            iss >> v3 >> slash >> slash >> trash;
+            // Assuming vertices are 1-indexed in .obj files, convert to 0-indexed
+            v1--; v2--; v3--;
+            faceData.push_back(vertices[v1].x);
+            faceData.push_back(vertices[v1].y);
+            faceData.push_back(vertices[v1].z);
+            faceData.push_back(vertices[v2].x);
+            faceData.push_back(vertices[v2].y);
+            faceData.push_back(vertices[v2].z);
+            faceData.push_back(vertices[v3].x);
+            faceData.push_back(vertices[v3].y);
+            faceData.push_back(vertices[v3].z);
+        }
+    }
+    file.close();
+    
+    return faceData;
 }
