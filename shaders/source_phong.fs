80659195d617955ced7c8d94411b28ec5c2eb1ca
darango3002 <donaldarango@ufl.edu>
2024-04-06 01:03:19 -0400

    added specular

--- shaders/source_phong.fs
+++ shaders/source_phong.fs
@@ -14,15 +14,22 @@
       float distance = length(LightPos - Pos);
   
       // Get a lighting direction vector from the light to the vertex.
-      vec3 lightVector = normalize(LightPos - Pos);
+      vec3 lightDir = normalize(LightPos - Pos);
+
+      float ambient = 0.3;
   
       // Calculate the dot product of the light vector and vertex normal. If the normal and light vector are
       // pointing in the same direction then it will get max illumination.
-      float diffuse = max(dot(Normal, lightVector), 0.1);
-  
+      float diffuse = max(dot(Normal, lightDir), 0.1);
       // Add attenuation.
       diffuse = diffuse * (1.0 / (1.0 + (0.25 * distance * distance)));
+
+      vec3 viewDir = normalize(vec3(0) - Pos);
+      vec3 reflectDir = reflect(-lightDir, Normal);
+
+      float spec = pow(max(dot(viewDir, reflectDir), 0.0), 32);
+      float specular = 0.3 * spec;  
   
       // Multiply the color by the diffuse illumination level to get final output color.
-      FragColor = vec4(Color * diffuse, 1.0f);
+      FragColor = vec4(Color * (ambient + diffuse + specular), 1.0f);
     }
